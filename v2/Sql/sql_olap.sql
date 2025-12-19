4a101e7ef3821004e206b5719bd61d2e98de5924
Miguel Samori de Azevedo <miguelsamzevedo@gmail.com>
2025-12-19 17:46:21 -0300

    feat: cria servidor com rota /health

--- v2/Sql/sql_olap.sql
+++ v2/Sql/sql_olap.sql
@@ -6,3 +6,17 @@ SELECT
 FROM f_matricula_especial fm
 JOIN dim_escola e
   ON e.co_entidade = fm.co_entidade;
+
+
+--SEED: dados fake apenas para testes de OLAP e mostrar aos colegas
+
+INSERT INTO f_matricula_especial
+  (co_entidade, cod_municipio_ibge, ano, cod_def, em_classe_comum, qtd_matriculas)
+VALUES
+  (1001, '3550308', 2023, 'DEF_AUDITIVA', TRUE, 10),
+  (1001, '3550308', 2023, 'SURDEZ', TRUE, 5);
+
+INSERT INTO f_recursos_acessibilidade
+  (co_entidade, ano, recurso_tipo, qtd_recursos)
+VALUES
+  (1001, 2023, 'INTERPRETE_LIBRAS', 1);
